262f9b75d69370de939f2b364614aaf42303262f
Nathalia Melo <whoisnathaliamelo@gmail.com>
2025-09-02 13:39:30 -0300

    Escondendo e removendo elementos com DOM

--- Js/DOM/DOM_saudacao/saudacao.js
+++ Js/DOM/DOM_saudacao/saudacao.js
@@ -1,5 +1,10 @@
-(function (){
-    const userName = " Nath"
-    document.querySelector(".top-bar p ").textContent += userName;
-})()
-
+(function () {
+  const userName =  ;
+  const elemento = document.querySelector(".top-bar p ");
+  if (userName) {
+    elemento.textContent += userName;
+  } else {
+    elemento.parentElement.style.display = "none"; // PARA ESCONDER
+    //       elemento.remove() // PARA REMOVER
+  }
+})();
