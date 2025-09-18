99fb71242b1c23948fd48e3aa7d1c24edf14c299
Nathalia Melo <whoisnathaliamelo@gmail.com>
2025-09-17 22:29:00 -0300

    finalização exercicio proposto

--- Js/Objetos/todo-list_inicio (2)/js/todolist.js
+++ Js/Objetos/todo-list_inicio (2)/js/todolist.js
@@ -1,185 +1,186 @@
-; (function () {
-    "use strict"
+(function () {
+  "use strict";
 
-
-
-	function Task(name, completed, createdAt, updatedAt){
-		// crie uma funcao construtora chamada Task. 
-        // essa funcao recebe por parametro obrigatório o nome da tarefa
-        // também recebe tres parametros opcionais (completed, createdAt, updatedAt)
-        // o objeto retornado por essa funcao deve ter quatro propriedades:
-        //  - name - string - obrigatório, 
-        //  - completed - boolean - opcional, false é o default, 
-        //  - createdAt - timestamp - opcional, timestamp atual é o valor default) 
-        //  - updatedAt - timestamp - opcional, null é o valor default
-        // o objeto retornado por essa funcao deve ter um método chamado toggleDone, que deve inverter o boolean completed
-	}
-
-	let arrTasks = [
-		{
-			name: "task 1",
-			completed: true,
-			createdAt: 1592667375012,
-			updatedAt: null
-		},
-		{
-			name: "task 2",
-			createdAt: 1581667345723,
-			updatedAt: 1592667325018
-		},
-		{
-			name: "task 3",
-			completed: true,
-			createdAt: 1592667355018,
-			updatedAt: 1593677457010
-		}
-	]
-
-
-    // a partir de um array de objetos literais, crie um array contendo instancias de Tasks. 
-    // Essa array deve chamar arrInstancesTasks
-	// const arrInstancesTasks = DESCOMENTE ESSA LINHA E RESOLVA O ENUNCIADO
-
-
-
-    //ARMAZENAR O DOM EM VARIAVEIS
-    const itemInput = document.getElementById("item-input")
-    const todoAddForm = document.getElementById("todo-add")
-    const ul = document.getElementById("todo-list")
-    const lis = ul.getElementsByTagName("li")
-
-
-    function generateLiTask(obj) {
-
-        const li = document.createElement("li")
-        const p = document.createElement("p")
-        const checkButton = document.createElement("button")
-        const editButton = document.createElement("i")
-        const deleteButton = document.createElement("i")
-
-        li.className = "todo-item"
-
-        checkButton.className = "button-check"
-        checkButton.innerHTML = `
-            <i class="fas fa-check ${obj.completed ? "" : "displayNone"}" data-action="checkButton"></i>`
-        checkButton.setAttribute("data-action", "checkButton")
-
-        li.appendChild(checkButton)
-
-        p.className = "task-name"
-        p.textContent = obj.name
-        li.appendChild(p)
-
-        editButton.className = "fas fa-edit"
-        editButton.setAttribute("data-action", "editButton")
-        li.appendChild(editButton)
-
-
-        const containerEdit = document.createElement("div")
-        containerEdit.className = "editContainer"
-        const inputEdit = document.createElement("input")
-        inputEdit.setAttribute("type", "text")
-        inputEdit.className = "editInput"
-        inputEdit.value = obj.name
-
-        containerEdit.appendChild(inputEdit)
-        const containerEditButton = document.createElement("button")
-        containerEditButton.className = "editButton"
-        containerEditButton.textContent = "Edit"
-        containerEditButton.setAttribute("data-action", "containerEditButton")
-        containerEdit.appendChild(containerEditButton)
-        const containerCancelButton = document.createElement("button")
-        containerCancelButton.className = "cancelButton"
-        containerCancelButton.textContent = "Cancel"
-        containerCancelButton.setAttribute("data-action", "containerCancelButton")
-        containerEdit.appendChild(containerCancelButton)
-
-        li.appendChild(containerEdit)
-
-
-
-        deleteButton.className = "fas fa-trash-alt"
-        deleteButton.setAttribute("data-action", "deleteButton")
-        li.appendChild(deleteButton)
-
-        return li
+  function Task(name, completed, createdAt, updatedAt) {
+    if (!name) {
+      throw new Error("Task precisa de um parâmetro obrigatório: name");
     }
 
-    function renderTasks() {
-        ul.innerHTML = ""
-        arrInstancesTasks.forEach(taskObj => {
-            ul.appendChild(generateLiTask(taskObj))
-        });
-    }
+    let _name = name;
+    this.getName = () => _name;
+    this.setName = function (name) {
+      _name = name;
+      this.updatedAt = Date.now();
+    };
 
-    function addTask(task) {
-        // adicione uma nova instancia de Task
-        renderTasks()
+    this.completed = completed || false;
+    this.createdAt = createdAt || Date.now();
+    this.updatedAt = updatedAt || null;
 
-    }
+    this.toggleDone = function () {
+      this.completed = !this.completed;
+    };
+  }
 
-    function clickedUl(e) {
-        const dataAction = e.target.getAttribute("data-action")
-        console.log(e.target)
-        if (!dataAction) return
+  let arrTasks = [
+    {
+      name: "task 1", // Corrigido de setName para name
+      completed: true,
+      createdAt: 1592667375012,
+      updatedAt: null,
+    },
+    {
+      name: "task 2", // Corrigido de setName para name
+      createdAt: 1581667345723,
+      updatedAt: 1592667325018,
+    },
+    {
+      name: "task 3", // Corrigido de setName para name
+      completed: true,
+      createdAt: 1592667355018,
+      updatedAt: 1593677457010,
+    },
+  ];
 
-        let currentLi = e.target
-        while (currentLi.nodeName !== "LI") {
-            currentLi = currentLi.parentElement
-        }
-        const currentLiIndex = [...lis].indexOf(currentLi)
+  // Criando instâncias de Task a partir do array de objetos literais
+  const arrInstancesTasks = arrTasks.map(function (task) {
+    return new Task(
+      task.name, // Ajuste aqui para `name` e não `setName`
+      task.completed,
+      task.createdAt,
+      task.updatedAt
+    );
+  });
 
-        const actions = {
-            editButton: function () {
-                const editContainer = currentLi.querySelector(".editContainer");
+  // ARMAZENAR O DOM EM VARIÁVEIS
+  const itemInput = document.getElementById("item-input");
+  const todoAddForm = document.getElementById("todo-add");
+  const ul = document.getElementById("todo-list");
+  const lis = ul.getElementsByTagName("li");
 
-                [...ul.querySelectorAll(".editContainer")].forEach(container => {
-                    container.removeAttribute("style")
-                });
+  function generateLiTask(obj) {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    const checkButton = document.createElement("button");
+    const editButton = document.createElement("i");
+    const deleteButton = document.createElement("i");
 
-                editContainer.style.display = "flex";
+    li.className = "todo-item";
 
+    checkButton.className = "button-check";
+    checkButton.innerHTML = ` 
+        <i class="fas fa-check ${
+          obj.completed ? "" : "displayNone"
+        }" data-action="checkButton"></i>`;
+    checkButton.setAttribute("data-action", "checkButton");
 
-            },
-            deleteButton: function () {
-                arrInstancesTasks.splice(currentLiIndex, 1)
-                renderTasks()
+    li.appendChild(checkButton);
 
-            },
-            containerEditButton: function () {
-                const val = currentLi.querySelector(".editInput").value
-                arrInstancesTasks[currentLiIndex].name = val
-                renderTasks()
-            },
-            containerCancelButton: function () {
-                currentLi.querySelector(".editContainer").removeAttribute("style")
-                currentLi.querySelector(".editInput").value = arrInstancesTasks[currentLiIndex].name
-            },
-            checkButton: function () {
+    p.className = "task-name";
+    p.textContent = obj.getName(); // Alterado de setName para getName
+    li.appendChild(p);
 
-                // DEVE USAR O MÉTODO toggleDone do objeto correto
+    editButton.className = "fas fa-edit";
+    editButton.setAttribute("data-action", "editButton");
+    li.appendChild(editButton);
 
-	            renderTasks()
-            }
-        }
+    const containerEdit = document.createElement("div");
+    containerEdit.className = "editContainer";
+    const inputEdit = document.createElement("input");
+    inputEdit.setAttribute("type", "text");
+    inputEdit.className = "editInput";
+    inputEdit.value = obj.getName(); // Alterado de setName para getName
 
-        if (actions[dataAction]) {
-            actions[dataAction]()
-        }
-    }
+    containerEdit.appendChild(inputEdit);
+    const containerEditButton = document.createElement("button");
+    containerEditButton.className = "editButton";
+    containerEditButton.textContent = "Edit";
+    containerEditButton.setAttribute("data-action", "containerEditButton");
+    containerEdit.appendChild(containerEditButton);
+    const containerCancelButton = document.createElement("button");
+    containerCancelButton.className = "cancelButton";
+    containerCancelButton.textContent = "Cancel";
+    containerCancelButton.setAttribute("data-action", "containerCancelButton");
+    containerEdit.appendChild(containerCancelButton);
 
-    todoAddForm.addEventListener("submit", function (e) {
-        e.preventDefault()
-        console.log(itemInput.value)
-        addTask(itemInput.value)
-        renderTasks()
+    li.appendChild(containerEdit);
 
-        itemInput.value = ""
-        itemInput.focus()
+    deleteButton.className = "fas fa-trash-alt";
+    deleteButton.setAttribute("data-action", "deleteButton");
+    li.appendChild(deleteButton);
+
+    return li;
+  }
+
+  function renderTasks() {
+    ul.innerHTML = "";
+    arrInstancesTasks.forEach((taskObj) => {
+      ul.appendChild(generateLiTask(taskObj));
     });
+  }
 
-    ul.addEventListener("click", clickedUl)
+  function addTask(task) {
+    arrInstancesTasks.push(new Task(task));
+    renderTasks();
+  }
 
-    renderTasks()
+  function clickedUl(e) {
+    const dataAction = e.target.getAttribute("data-action");
+    console.log(e.target);
+    if (!dataAction) return;
 
+    let currentLi = e.target;
+    while (currentLi.nodeName !== "LI") {
+      currentLi = currentLi.parentElement;
+    }
+    const currentLiIndex = [...lis].indexOf(currentLi);
+
+    const actions = {
+      editButton: function () {
+        const editContainer = currentLi.querySelector(".editContainer");
+
+        [...ul.querySelectorAll(".editContainer")].forEach((container) => {
+          container.removeAttribute("style");
+        });
+
+        editContainer.style.display = "flex";
+      },
+      deleteButton: function () {
+        arrInstancesTasks.splice(currentLiIndex, 1);
+        renderTasks();
+      },
+      containerEditButton: function () {
+        const val = currentLi.querySelector(".editInput").value;
+        arrInstancesTasks[currentLiIndex].setName(val); // Corrigido para usar o setName
+        renderTasks();
+      },
+      containerCancelButton: function () {
+        currentLi.querySelector(".editContainer").removeAttribute("style");
+        currentLi.querySelector(".editInput").value =
+          arrInstancesTasks[currentLiIndex].getName();
+      },
+      checkButton: function () {
+        arrInstancesTasks[currentLiIndex].toggleDone();
+        renderTasks();
+      },
+    };
+
+    if (actions[dataAction]) {
+      actions[dataAction]();
+    }
+  }
+
+  todoAddForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log(itemInput.value);
+    addTask(itemInput.value);
+    renderTasks();
+
+    itemInput.value = "";
+    itemInput.focus();
+  });
+
+  ul.addEventListener("click", clickedUl);
+
+  renderTasks();
 })();
